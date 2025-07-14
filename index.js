9aa040dfcbd74261bb00a6293f834527a8ad1a05
Calks <68165466+Calitts@users.noreply.github.com>
2025-07-14 00:19:08 -0300

    Pre-release

--- index.js
+++ index.js
@@ -38,33 +38,122 @@ function fetchLink() {
       url = "https://" + url;
     }
     console.log(url);
-    data = fetch(
-      `https://egapi.onrender.com/api/scan?url=${url}&verif=0`
-    )
+    data = fetch(`https://egapi.onrender.com/api/scan?url=${url}&verif=0`)
       .then((res) => res.json())
       .then((data) => redirect(data))
       .catch((error) => console.log(error));
   }
 }
 
-
 // Response Pages
 
+function nerds() {
+  
+}
+
 function getJSON() {
-    data = JSON.parse(sessionStorage.getItem("siteData"));
-    //console.log(data);
-    document.getElementById("box-right").innerHTML = data.site;
-    return data;
+  data = JSON.parse(sessionStorage.getItem("siteData"));
+  //console.log(data);
+  //document.getElementById("box-right").innerHTML = data.site;
+  return data;
 }
 
 function displayJSON(data) {
-    string = "";
-    console.log(data)
-    for (key in data) {
-        if (data.hasOwnProperty(key)) {
-          string += `${key}: ${data[key]}<br>`
-        }
+  stringTag = "";
+  stringInfo = "";
+  websiteName = "";
+  console.log(data);
+
+  tag = document.getElementById("inner-left");
+  info = document.getElementById("inner-right");
+  for (key in data) {
+    if (data.hasOwnProperty(key)) {
+      switch (key) {
+        case "ano_criacao":
+          stringTag += "Ano de Criação:<br>";
+          stringInfo += data[key] + "<br>";
+          break;
+        case "tempo_de_existencia":
+          stringTag += "Tempo de existência:<br>";
+          stringInfo += data[key] + "<br>";
+          break;
+        case "hospedado":
+          stringTag += "O site utiliza o servidor:<br>";
+          stringInfo +=
+            data[key].replace("O site utiliza o servidor: ", "") + "<br>";
+          break;
+        case "ssl":
+          stringTag += "Certificado SSL:<br>";
+          temp =
+            data[key].replace("O website ", "").charAt(0).toUpperCase() +
+            data[key].replace("O website ", "").slice(1);
+          stringInfo += temp + "<br>";
+          break;
+        case "vulneravel_a_alteracao":
+          if (data[key] == "Sem scan") {
+            break;
+          }
+          stringTag += "O site é vulnerável<br>";
+          stringInfo += "<br>";
+          break;
+        case "vulneravel_a_dataleak":
+          if (data[key] == "Sem scan") {
+            break;
+          }
+          stringTag += "O dados são vulneráveis<br>";
+          stringInfo += "<br>";
+          break;
+        case "tipo_de_uso":
+          stringTag += "Tipo de uso:<br>";
+          stringInfo += data[key] + "<br>";
+          break;
+        case "whois":
+          nerds();
+          break;
+        case "download":
+          stringTag += "O site faz download:<br>";
+          if (data[key]["tipo_de_arquivo"] == null) {
+            stringInfo += data[key]["content_type"] + "<br>";
+          } else {
+            stringInfo += "O site baixa arquivos<br>";
+          }
+          break;
+        case "site":
+          websiteName = data[key]
+            .replace("https://", "")
+            .replace("http://", "");
+          break;
+        case "nota_dos_usuarios":
+          if (data[key] == null) {
+            break;
+          }
+          stringTag += "Classificação por usuários:<br>";
+          stringInfo += data[key] + "<br>";
+          break;
+        case "ip_total_reports":
+          stringTag += "Esse ip foi reportado:<br>";
+          if (data[key] != "1") {
+            stringInfo += data[key] + " vezes<br>";
+          } else {
+            stringInfo += data[key] + " veze<br>";
+          }
+          break;
+      }
     }
-    // console.log(string)
-    document.getElementById("box-right").innerHTML = string;
+  }
+
+  nota = data["nota_total"];
+  if (nota[1] == 0) {
+    nota = "10";
+  } else {
+    nota = nota[0];
+  }
+
+  nota = parseInt(nota);
+  nota = 10 - nota;
+
+  info.innerHTML = stringInfo;
+  tag.innerHTML = stringTag;
+  document.getElementById("website-name").innerHTML = websiteName;
+  document.getElementById("nota").innerHTML = `${nota}/10`;
 }
